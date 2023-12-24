6c278d799c5694eefb904644525d855c55b02788
Mayk Brito <maykbrito@gmail.com>
2023-12-24 08:07:22 -0300

    add shadcn-ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		react(), 
+		tailwind({
+			applyBaseStyles: false,
+		})
+	]
+})
